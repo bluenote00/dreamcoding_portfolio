ae7280d6ff7a4bf6f8d4db07c8aad71ba6e67550
annej <god0937@gmail.com>
2023-04-04 01:44:28 +0900

    JS_Contact

--- main.js
+++ main.js
@@ -25,21 +25,22 @@ navbarMenu.addEventListener('click', (event) => {
         return;
     }
 
-    console.log(event.target.dataset.link);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({ behavior: 'smooth' });
+    //console.log(event.target.dataset.link);
+    //const scrollTo = document.querySelector(link);
+    //scrollTo.scrollIntoView({ behavior: 'smooth' });
+
+    scrollIntoView(link);
 });
 
 // Contact me 메뉴 이동
-const contact = document.querySelector('.home__contact');
-contact.addEventListener('click', (event) => {
-    const target = event.target;
-    const link = target.dataset.link;
-    if (link == null) {
-        return;
-    }
-
-    console.log(event.target.dataset.link);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({ behavior: 'smooth' });
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+    //const scrollTo = document.querySelector('#contact');
+    //scrollTo.scrollIntoView({ behavior: 'smooth' });
+    scrollIntoView('#contact');
 });
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({ behavior: 'smooth' });
+}
