b20b967ba59ed051ebd99c97514e3f56911b96a2
annej <god0937@gmail.com>
2023-04-05 19:30:34 +0900

    JS_selected

--- main.js
+++ main.js
@@ -78,12 +78,20 @@ const projects = document.querySelectorAll('.project');
         if(filter == null) {
             return;
         }
-        projectContainer.classList.add('anim-out');
 
+        //Remove selection
+        const active = document.querySelector('.category__btn.selected');
+        active.classList.remove('selected');
+        const target = 
+            e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+        e.target.classList.add('selected');
+
+
+        projectContainer.classList.add('anim-out');
         setTimeout(() => {
-            projects.forEach((project) => {
+            projects.forEach(project => {
                 console.log(project.dataset.type);
-                if(filter === '*' || filter === project.dataset.type) {
+                if (filter === '*' || filter === project.dataset.type) {
                     project.classList.remove('invisible');
                 } else {
                     project.classList.add('invisible');
